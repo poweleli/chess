7f048d01cb89b4bd5d20f9e2341f28aec8356029
epowell <elizapowell00@gmail.com>
2024-07-15 15:53:04 -0600

    model classes

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,20 @@
+package model;
+
+import chess.ChessGame;
+
+public class GameData {
+    private int gameID;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+    private ChessGame game;
+
+    public GameData(int gameID, String whiteUsername, String blackUsername, String gameName) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = new ChessGame();
+    }
+
+}
