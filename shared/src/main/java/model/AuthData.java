cebfb3762fd6edf82f08738a4ca0ccec7bb02207
epowell <elizapowell00@gmail.com>
2024-07-15 16:03:24 -0600

    model classes fixed to records

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,22 +1,4 @@
 package model;
 
-import java.util.Random;
-import java.util.Base64;
-
-public class AuthData {
-    private String username;
-    private String authToken;
-    private static final Random random = new Random(); // Random instance
-    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
-
-    public AuthData(String username) {
-        this.username = username;
-        this.authToken = generateToken();
-    }
-
-    public static String generateToken() {
-        byte[] randomBytes = new byte[24];
-        random.nextBytes(randomBytes); // Generate 24 random bytes
-        return base64Encoder.encodeToString(randomBytes); // Convert to a base64 string
-    }
+public record AuthData(String username, String authToken) {
 }
