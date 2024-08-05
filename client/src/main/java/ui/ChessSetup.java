99081d330f7bd6559705fb110c122012887e3ec7
epowell <elizapowell00@gmail.com>
2024-08-05 16:30:06 -0600

    chess board

---
+++ client/src/main/java/ui/ChessSetup.java
@@ -0,0 +1,4 @@
+package ui;
+
+public record ChessSetup(String name, String[] rowLabels, String[] colLabels, String[] pieceOrder, String myColor, String otherColor) {
+}
--- client/src/main/java/ui/DrawChess.java
+++ client/src/main/java/ui/DrawChess.java
@@ -2,7 +2,6 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 import static ui.EscapeSequences.*;
 
@@ -18,38 +17,55 @@ public class DrawChess {
     private static final String X = " X ";
     private static final String O = " O ";
 
+    private static final String[] colsBlack = {"8", "7", "6", "5", "4", "3", "2", "1"};
     private static final String[] headersBlack = { "a", "b", "c", "d", "e", "f", "g", "h" };
+    private static final String[] colsWhite = { "1", "2", "3", "4", "5", "6", "7", "8" };
     private static final String[] headersWhite = {"h", "g", "f", "e", "d", "c", "b", "a"};
-
-    private static Random rand = new Random();
+    // Piece representations
+    private static final String[] INITIAL_ROW_BLACK  = {"R", "N", "B", "Q", "K", "B", "N", "R"};
+    private static final String[] INITIAL_ROW_WHITE = {"R", "N", "B", "K", "Q", "B", "N", "R"};
 
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         out.print(ERASE_SCREEN);
+        ChessSetup chessSetup = new ChessSetup("black",
+                new String[]{"1", "2", "3", "4", "5", "6", "7", "8"},
+                new String[]{"h", "g", "f", "e", "d", "c", "b", "a" },
+                new String[]{"R", "N", "B", "K", "Q", "B", "N", "R"},
+                SET_TEXT_COLOR_RED,
+                SET_TEXT_COLOR_BLUE);
+        drawBoard(out, chessSetup);
+        out.println();
+        ChessSetup chessSetup2 = new ChessSetup("white",
+                new String[]{"8", "7", "6", "5", "4", "3", "2", "1"},
+                new String[]{ "a", "b", "c", "d", "e", "f", "g", "h" },
+                new String[]{"R", "N", "B", "Q", "K", "B", "N", "R"},
+                SET_TEXT_COLOR_BLUE,
+                SET_TEXT_COLOR_RED);
+        drawBoard(out, chessSetup2);
 
-        drawHeaderFooter(out);
-
-        drawTicTacToeBoard(out);
-
-        drawHeaderFooter(out);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void drawHeaderFooter(PrintStream out) {
+    private static void drawBoard(PrintStream out, ChessSetup chessSetup){
+        drawHeaderFooter(out, chessSetup);
+        drawTicTacToeBoard(out, chessSetup);
+        drawHeaderFooter(out, chessSetup);
+    }
+
+    private static void drawHeaderFooter(PrintStream out, ChessSetup chessSetup) {
 
         setBlack(out);
 
-
+        out.print(" ".repeat(3));
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headersBlack[boardCol]);
+            drawHeader(out, chessSetup.colLabels()[boardCol]);
 
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-            }
+            out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
         }
 
         out.println();
@@ -70,64 +86,62 @@ public class DrawChess {
         setBlack(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void drawTicTacToeBoard(PrintStream out, ChessSetup chessSetup) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+            drawRowHeaderFooter(out, boardRow, chessSetup);
+            drawRowOfSquares(out, boardRow, chessSetup);
 
-            drawRowOfSquares(out);
-
-            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-                // Draw horizontal row separator.
-                drawHorizontalLine(out);
-                setBlack(out);
-            }
         }
     }
 
-    private static void drawRowOfSquares(PrintStream out) {
+    private static void drawRowHeaderFooter(PrintStream out, int boardRow, ChessSetup chessSetup) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+        out.print(" " + chessSetup.rowLabels()[boardRow] + " ");
+    }
 
+    private static void getPlayer(PrintStream out, int boardRow, int boardCol, ChessSetup chessSetup) {
+        if (boardRow == 0 || boardRow == 1) {
+            out.print(chessSetup.myColor());
+            if (boardRow == 0) {
+                out.print(" " + chessSetup.pieceOrder()[boardCol]+ " ");
+            } else {
+                out.print(" " + "p"+ " ");
+            }
+        } else if (boardRow == 6 || boardRow == 7) {
+            out.print(chessSetup.otherColor());
+            if (boardRow == 7) {
+                out.print(" " + chessSetup.pieceOrder()[boardCol]+ " ");
+            } else {
+                out.print(" " + "p" + " ");
+            }
+
+        } else {
+            out.print(EMPTY);
+        }
+    }
+
+    private static void drawRowOfSquares(PrintStream out, int boardRow, ChessSetup chessSetup) {
         for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                setWhite(out);
-
-                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-
-                    out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
-                    out.print(EMPTY.repeat(suffixLength));
+                if ((boardCol % 2 == 0 && boardRow % 2 == 1) ||
+                        (boardCol % 2 == 1 && boardRow%2 == 0)) {
+                    out.print(SET_BG_COLOR_DARK_GREY);
+                } else {
+                    out.print(SET_BG_COLOR_LIGHT_GREY);
                 }
-                else {
-                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
-                }
+                getPlayer(out, boardRow, boardCol, chessSetup);
 
-                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                    // Draw vertical column separator.
-                    setRed(out);
-                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+                if (boardCol == BOARD_SIZE_IN_SQUARES-1) {
+                    drawRowHeaderFooter(out, boardRow, chessSetup);
                 }
-
                 setBlack(out);
             }
-
             out.println();
         }
     }
 
-    private static void drawHorizontalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
-        }
-    }
 
     private static void setWhite(PrintStream out) {
         out.print(SET_BG_COLOR_WHITE);
